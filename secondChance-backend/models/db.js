93062a418158c1c4221f8c4dd972e66ba032e6dd
Mohammad Rahimi <amrdesign.m@gmail.com>
2024-09-23 22:02:58 +0300

    api implemented

--- secondChance-backend/models/db.js
+++ secondChance-backend/models/db.js
@@ -1,6 +1,7 @@
-// db.js
-require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
+// connect to mongoDB
+
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
 
 // MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
@@ -9,20 +10,20 @@ let dbInstance = null;
 const dbName = `${process.env.MONGO_DB}`;
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
+	// if (dbInstance) {
+	// 	return dbInstance;
+	// }
 
-    const client = new MongoClient(url);      
+	const client = new MongoClient(url);
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
+	// Task 1: Connect to MongoDB
+	await client.connect();
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+	// Task 2: Connect to database giftDB and store in variable dbInstance
+	const dbInstance = client.db(dbName);
 
-    // Task 3: Return database instance
-    // {{insert code}}
+	// Task 3: Return database instance
+	return dbInstance;
 }
 
 module.exports = connectToDatabase;
